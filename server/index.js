45b37c9746b783c2fe8b558b6ad5308f4d3fc033
temirhanb <temirhan023@gmail.com>
2024-04-22 21:18:41 +0500

    test

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
-import express, {Router} from "express";
+import express from "express";
+import ViteExpress from "vite-express";
 import cors from "cors";
 import router from "./router.js";
 const app = express();
@@ -9,7 +10,10 @@ app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(router);
+ViteExpress.config({ mode: "production" })
 
 app.listen(PORT, () => {
   console.log('Server start on port: ' + PORT);
 });
+
+ViteExpress.listen(app, PORT, () => console.log("Server is listening..."));
