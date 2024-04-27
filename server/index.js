1bfad30e2493bacb284db54ccefe91d1cd79e7a8
temirhanb <temirhan023@gmail.com>
2024-04-27 17:13:27 +0500

    - fix err port server

--- server/index.js
+++ server/index.js
@@ -10,10 +10,7 @@ app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(router);
-ViteExpress.config({ mode: "production" })
 
 app.listen(PORT, () => {
   console.log('Server start on port: ' + PORT);
 });
-
-ViteExpress.listen(app, PORT, () => console.log("Server is listening..."));
